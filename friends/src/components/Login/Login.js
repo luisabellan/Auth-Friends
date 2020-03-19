1e1284228e27e542e54cf6cf308b3417f79108eb
squarerobin <squarerobin.82@gmail.com>
2020-03-19 09:46:44 +0100

    added isLoading state and styling

--- friends/src/components/Login/Login.js
+++ friends/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { axiosWithAuth } from "../../utils/axiosWithAuth";
 
 import './Login.css'
@@ -6,6 +6,7 @@ import './Login.css'
 const Login = (props) => {
 
   const [credentials, setCredentials] = useState({})
+  const[isLoading, setIsLoading] = useState(false)
 
 
 
@@ -13,18 +14,21 @@ const Login = (props) => {
   const login = e => {
     e.preventDefault();
     
+
     // add in our login api call
     axiosWithAuth()
     .post("/login", credentials)
     .then(res => {
       console.log(res);
       localStorage.setItem("token", res.data.payload);
+     
       // nice for UX, auto redirect to the main dash
       props.history.push("/friends");
     })
     .catch(err => {
       console.log(err);
     });
+   
   };
   
   const handleChange = e => {
